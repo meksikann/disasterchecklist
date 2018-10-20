9a132bd1f39bdbbe46b0f28008c58c981415e4f5
Aenqau <cauthraydon@gmail.com>
2018-10-20 20:06:10 +0300

    fire disaster page scaffolding

---
+++ web2/pages/FiresView.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-layout column justify-center align-center>
+    <!--<Speak></Speak>-->
+
+  </v-layout>
+</template>
+
+<style scoped>
+</style>
+
+<script>
+  import Speak from "../components/Speak";
+
+  export default {
+    components: {Speak}
+  }
+</script>
