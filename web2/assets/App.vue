d22b5abf33ca10953bab34d51050d56c94144289
Aenqau <cauthraydon@gmail.com>
2018-10-20 23:29:18 +0300

    fix router

--- web2/assets/App.vue
+++ web2/assets/App.vue
@@ -56,7 +56,7 @@
         fixed: false,
         items: [
           { icon: 'home', title: 'Welcome', to: '/' },
-          { icon: 'add_circle', title: 'Create a checklist', to: '/checklist' },
+          { icon: 'add_circle', title: 'Create a checklist', to: '/disasters' },
           { icon: 'search', title: 'View map', to: '/map' }
         ],
         miniVariant: false,
