f124d99bfe20769e976beadffd5e5791be5b833b
serhii <serhii.skoromets@eliftech.com>
2018-10-20 13:14:48 +0300

    srest api

--- server/server.js
+++ server/server.js
@@ -2,6 +2,15 @@ const express = require('express');
 const app = express()
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Disaster Assistant'))
+app.get('/', (req, res) => res.send('Disaster Assistant'));
+app.get('/checklist', (req, res) => {
+    console.log('get checklist');
+    }
+);
+
+app.get('/voice-command', (req, res) => {
+        console.log('check voice command');
+    }
+);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
