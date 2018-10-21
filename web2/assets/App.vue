2bd161c34c766e64177b1d1a6af558df3dfbf2e2
Aenqau <cauthraydon@gmail.com>
2018-10-21 05:32:09 +0300

    return of the menu

--- web2/assets/App.vue
+++ web2/assets/App.vue
@@ -1,37 +1,37 @@
 <template>
   <v-app light>
-    <!--<v-navigation-drawer-->
-      <!--:mini-variant="miniVariant"-->
-      <!--:clipped="clipped"-->
-      <!--v-model="drawer"-->
-      <!--fixed-->
-      <!--app-->
-    <!--&gt;-->
-      <!--<v-list>-->
-        <!--<v-list-tile-->
-          <!--router-->
-          <!--:to="item.to"-->
-          <!--:key="i"-->
-          <!--v-for="(item, i) in items"-->
-          <!--exact-->
-        <!--&gt;-->
-          <!--<v-list-tile-action>-->
-            <!--<v-icon v-html="item.icon"></v-icon>-->
-          <!--</v-list-tile-action>-->
-          <!--<v-list-tile-content>-->
-            <!--<v-list-tile-title v-text="item.title"></v-list-tile-title>-->
-          <!--</v-list-tile-content>-->
-        <!--</v-list-tile>-->
-      <!--</v-list>-->
-    <!--</v-navigation-drawer>-->
-    <!--<v-toolbar fixed class="main">-->
-      <!--<v-toolbar-side-icon dark @click.stop="drawer = !drawer"></v-toolbar-side-icon>-->
-    <!--</v-toolbar>-->
-    <!--<v-toolbar dark fixed app :clipped-left="clipped" class="main">-->
-      <!--<v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>-->
-      <!--<v-toolbar-title v-text="title"></v-toolbar-title>-->
-      <!--<v-spacer></v-spacer>-->
-    <!--</v-toolbar>-->
+    <v-navigation-drawer
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      v-model="drawer"
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-tile
+          router
+          :to="item.to"
+          :key="i"
+          v-for="(item, i) in items"
+          exact
+        >
+          <v-list-tile-action>
+            <v-icon v-html="item.icon"></v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="item.title"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar fixed class="main">
+      <v-toolbar-side-icon dark @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+    </v-toolbar>
+    <v-toolbar dark fixed app :clipped-left="clipped" class="main">
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title v-text="title"></v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-toolbar>
 
     <speak></speak>
     <v-content :class="{ 'disasters': $route.path == '/disasters' }">
